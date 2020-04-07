d796b9e1d11319bc2c4e23f62e09705eb6c69618
André Mathisen <waremanu@gmail.com>
2020-04-07 15:16:56 +0200

    Release 2.1

--- src/no/spillere/oreregen/util/Paper.java
+++ src/no/spillere/oreregen/util/Paper.java
@@ -8,7 +8,7 @@ import no.spillere.oreregen.OreRegeneration;
 public class Paper {
 
 	public static void generateOreVein(World world, int x, int z, Material type, int veinSize) {
-		world.getChunkAtAsync(x, z, true, (chunk) -> {
+		world.getChunkAtAsync(x, z, false, (chunk) -> {
 			OreRegeneration.instance.OreRegenHandler.generateOreVein(chunk, type, veinSize);
 		});
 	}
